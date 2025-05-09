badeb889d829cc456b2ec90e38c9fd1ac5009981
smchoi <seonmun.choi@gmail.com>
2025-05-09 12:16:47 +0900

    게임 결과 판정 구현

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -24,6 +24,7 @@ public class GameManager : MonoBehaviour
     // 현재 턴
     // ㄴ 입력에 대해 이 데이터를 가지고 출력을 처리해야 한다.
     private SquareState _currentTurnState = SquareState.Cross;
+    private GameOverState _gameOverState = GameOverState.NotOver;
 
     // 출력을 위해서는 보드의 상태(칸의 좌표, 칸의 상태)가 변경됐다는 것을 알려야 한다.
     public event Action<int, int, SquareState> OnBoardChanged;
@@ -31,6 +32,11 @@ public class GameManager : MonoBehaviour
     
     public void PlayMarker(int x, int y)
     {
+        if (_gameOverState != GameOverState.NotOver)
+        {
+            return;
+        }
+
         if (_board[y, x] != SquareState.None)
         {
             return;
@@ -38,15 +44,16 @@ public class GameManager : MonoBehaviour
 
         _board[y, x] = _currentTurnState;
 
-        Logger.Info("보드의 상태");
-        for (int line = 0; line < 3; ++line)
-        {
-            Logger.Info($"{_board[line, 0]}, {_board[line, 1]}, {_board[line, 2]}");
-        }
-
         // 구독한 객체에게 보드의 상태가 바뀌었다는 것을 통지한다.
         OnBoardChanged?.Invoke(x, y, _currentTurnState);
 
+        _gameOverState = TestGameOver();
+        if (_gameOverState != GameOverState.NotOver)
+        {
+            Logger.Info($"{_gameOverState} is winner.");
+            return;
+        }
+        
         if (_currentTurnState == SquareState.Cross)
         {
             _currentTurnState = SquareState.Circle;
@@ -55,6 +62,93 @@ public class GameManager : MonoBehaviour
         {
             _currentTurnState = SquareState.Cross;
         }
+    }
+
+    // O가 승자, X가 승자, 무승부, 아직 게임이 끝나지 않음
+    enum GameOverState
+    {
+        NotOver,
+        Cross,
+        Circle,
+        Tie
+    }
+
+    GameOverState TestGameOver()
+    {
+        // 가로 검사
+        for (int y = 0; y < 3; ++y)
+        {
+            if (_board[y, 0] != SquareState.None &&
+            _board[y, 0] == _board[y, 1] && _board[y, 1] == _board[y, 2])
+            {
+                if (_board[y, 0] == SquareState.Cross)
+                {
+                    return GameOverState.Cross;
+                }
+                else if (_board[y, 0] == SquareState.Circle)
+                {
+                    return GameOverState.Circle;
+                }
+            }
+        }
+
+        // 세로 검사
+        for (int x = 0; x < 3; ++x)
+        {
+            if (_board[0, x] != SquareState.None &&
+            _board[0, x] == _board[1, x] && _board[1, x] == _board[2, x])
+            {
+                if (_board[0, x] == SquareState.Cross)
+                {
+                    return GameOverState.Cross;
+                }
+                else if (_board[0, x] == SquareState.Circle)
+                {
+                    return GameOverState.Circle;
+                }
+            }
+        }
+
+        // 대각선 검사
+        if (_board[0, 0] != SquareState.None &&
+            _board[0, 0] == _board[1, 1] && _board[1, 1] == _board[2, 2])
+        {
+            if (_board[0, 0] == SquareState.Cross)
+            {
+                return GameOverState.Cross;
+            }
+            else if (_board[0, 0] == SquareState.Circle)
+            {
+                return GameOverState.Circle;
+            }
+        }
+        if (_board[2, 0] != SquareState.None &&
+            _board[2, 0] == _board[1, 1] && _board[1, 1] == _board[0, 2])
+        {
+            if (_board[2, 0] == SquareState.Cross)
+            {
+                return GameOverState.Cross;
+            }
+            else if (_board[2, 0] == SquareState.Circle)
+            {
+                return GameOverState.Circle;
+            }
+        }
+
+        // 무승부 : 보드의 모든 칸이 채워졌음에도 한 쪽이 승리가 나지 않은 경우
+        // ㄴ 모든 칸이 None이 아닌 것으로 판별할 수 있다.
+        for (int y = 0; y < 3; ++y)
+        {
+            for (int x = 0; x < 3; ++x)
+            {
+                if (_board[y, x] == SquareState.None)
+                {
+                    return GameOverState.NotOver;
+                }
+            }
+        }
+
+        return GameOverState.Tie;
     }
 
     private void Awake()
